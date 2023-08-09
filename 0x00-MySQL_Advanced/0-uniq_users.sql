97bc0d46e1c68447e29e72e6c04cdf41a7f43059
Lapololucy <lapololucy@gmail.com>
2023-08-09 07:54:12 +0000

    We are all unique!

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,6 @@
+-- Create a table named 'users' with specified attributes
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);
