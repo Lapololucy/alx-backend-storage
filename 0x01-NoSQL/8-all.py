4771804029797b9e5f2f61bdc7338417661c6b55
Lapololucy <lapololucy@gmail.com>
2023-08-15 08:16:10 -0700

    List all document in python

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""List all documents in Python
+"""
+
+def list_all(mongo_collection):
+    """List all documents in a collection
+    """
+    return mongo_collection.find()
